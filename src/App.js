30c3a1fbf90d16f5089101ad7fc82bab425cc315
Shivamjha12 <Jhasapna544@gmail.com>
2023-11-04 21:57:50 +0530

    Adding Home.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import SearchPage from './Pages/SearchPage';
+// import SearchPage from './Pages/SearchPage';
+import Home from './Pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <SearchPage/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Ellipse4 from "../Assets/Ellipse4.jpg"
+function Home(){
+    return(
+        <div className="HomePage"> 
+            <div className="HomePageDiv1">
+                <h1 style={{"color":"#fff","font-size":"60px","margin":"0px 0px 0px 370px"}}>Extra Power For Work</h1>
+            </div>
+            <div className="HeroImg1">
+                <img  src={Ellipse4} alt=""/>
+            </div>
+            <div className="HeroPara1" >
+                <p> 
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the
+                </p>
+            </div>
+            <div className="HeroImg2">
+                <img  src={Ellipse4} alt=""/>
+            </div>
+            <div className="HeroPara2" >
+                <p> 
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the ThingsExplain the ThingsExplain the
+                    ThingsExplain the
+                </p>
+            </div>
+        </div>
+    )
+}
+export default Home;

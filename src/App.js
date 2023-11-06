0afe904d52ed13938181ef892208070afab0dcb8
Shivamjha12 <Jhasapna544@gmail.com>
2023-11-07 00:07:36 +0530

    Adding Rating,Like,Share Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import SearchPage from './Pages/SearchPage';
+import Header from './Component/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <SearchPage/>
     </div>
   );
